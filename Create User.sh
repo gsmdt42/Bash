785c126744fa7b0427906ca8830c596c0aa316a9
Gianni <giannischmidt2001@gmail.com>
2024-11-15 13:54:44 +0100

    Update Create User.sh

--- Create User.sh
+++ Create User.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Benutzername und Passwort abfragen
+#Input for username and password
 read -p "Enter username: " username
 read -s -p "Enter password: " password
 echo
 
-# Benutzer hinzufügen
+#Add user and set password
 sudo adduser --disabled-password --gecos "" $username
 echo "$username:$password" | sudo chpasswd
 
-# Abfrage, ob der Benutzer sudo-Rechte haben soll
+#Request whether the user should be a superuser (sudo)
 read -p "Should the user have sudo privileges? (y/n): " sudo_choice
 if [[ "$sudo_choice" == "y" || "$sudo_choice" == "Y" ]]; then
     sudo usermod -aG sudo $username
-    echo "$username has been granted sudo privileges."
+    echo "User $username has been granted sudo privileges."
 else
-    echo "$username does not have sudo privileges."
+    echo "User $username does not have sudo privileges."
 fi
 
 echo "User $username has been created successfully."
